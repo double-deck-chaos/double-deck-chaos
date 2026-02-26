70e3621b1910c95faea80c1bebedc2b561cb9599
double-deck-chaos <marcosdatashow201316@gmail.com>
2026-02-26 19:32:25 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,21 @@
-Service worker Double Deck independent studio
+const CACHE_NAME = 'ddc-v1';
+const ASSETS = [
+  'index.html',
+  'ddc.html',
+  'cmm.html',
+  'crjr.html',
+  'wild-chaos.png',
+  'joker-chaos.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
